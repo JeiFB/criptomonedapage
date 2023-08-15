6a640149af21be3297d82d168dbf6f2da8e57b0c
JeiFB <fuentesjeifer@gmail.com>
2023-08-15 17:51:07 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id:'USD', nombre: 'Dolar de estados unidos'},
+    {id:'COP', nombre: 'Peso colombiano'},
+    {id:'EUR', nombre: 'EURO'},
+    {id:'GBP', nombre: 'Libra esterlina'},
+  ]
+
+  export{
+    monedas
+  }
